d7b309d365518b9a59e1c8f753d56435af6b274e
obed300 <obamfo@st.ug.edu.gh>
2023-05-12 09:56:31 +0000

    Add factorial

--- solver.rb
+++ solver.rb
@@ -1,2 +1,7 @@
 class Solver
+  def factorial(number)
+    return 1 if [0, 1].include?(number)
+
+    number * factorial(number - 1)
+  end
 end
--- specs/solver_spec.rb
+++ specs/solver_spec.rb
@@ -1,5 +1,21 @@
 require 'rspec'
-require_relative '../solver.rb'
+require_relative '../solver'
 
 describe Solver do
+  describe '#factorial' do
+    it 'takes an integer as argument and returns a factorial for that number' do
+      solve = Solver.new
+      num = 5
+      result = solve.factorial(num)
+      expected_factorial = 120
+      expect(result).to eql(expected_factorial)
+    end
+    it 'return 1 with a factorial of 0' do
+      solve = Solver.new
+      num = 0
+      result = solve.factorial(num)
+      expected_factorial = 1
+      expect(result).to eql(expected_factorial)
+    end
+  end
 end
